e94736d7650cbde09187cfb44914f13f4c306373
Gurumauj Satsangi <gurumaujsatsangi@gmail.com>
2026-03-10 21:16:59 +0530

    Refactor app.js for improved readability and update dashboard.ejs and home.ejs for enhanced user input validation and layout

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ import ejs from "ejs";
 import session from "express-session";
 import { fileURLToPath } from "url";
 import path from "path";
-import multer from 'multer';
-import { createClient } from '@supabase/supabase-js'
+import multer from "multer";
+import { createClient } from "@supabase/supabase-js";
 
 dotenv.config();
 
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY)
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY,
+);
 
 const app = express();
 app.use(express.static("public"));
@@ -19,31 +22,52 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
-
 const port = Number(process.env.PORT) || 5000;
 
-app.get("/",async(req,res)=>{
-    return res.render("home.ejs");
+app.get("/", async (req, res) => {
+  return res.render("home.ejs");
+});
+
+app.get("/dashboard", async (req, res) => {
+  const message = req.query;
+  const { data, error } = await supabase.from("checkin").select("*");
+
+  return res.render("dashboard.ejs", { data: data, message: message || null });
+});
+
+
+app.put("/edit/:id", async(req,res)=>{
+
+    const {scheduled_check_in_date, scheduled_check_in_time, luggae_info, image} = req.body;
+    const {data,error} = await supabase.from("checkin").update({scheduled_check_in_date:scheduled_check_in_date,scheduled_check_in_time:scheduled_check_in_time,luggage_info:luggae_info,image:image}).eq("id",req.params.id);
+    return res.redirect("/dashboard?message=Scheduled Check-In Updated Succesfully!");
 })
 
-app.get("/dashboard",async(req,res)=>{
-    const message = req.query;
-    const {data,error} = await supabase.from("checkin").select("*");
+app.post("/schedule-check-in", async (req, res) => {
+  const {
+    check_in_date,
+    check_out_date,
+    check_in_time,
+    check_out_time,
+    luggage_info,
+    image,
+  } = req.body;
 
-    return res.render("dashboard.ejs",{data,message: message || null });
-})
+  const { data, error } = await supabase
+    .from("checkin")
+    .insert({
+      scheduled_check_in_date: check_in_date,
+      scheduled_check_in_time: check_in_time,
+      luggage_info: luggage_info,
+    });
 
-app.post("/schedule-check-in", async(req,res)=>{
-    const {check_in_date, check_out_date, check_in_time, check_out_time, luggage_info, image} = req.body;
+  if (error) {
+    return res.redirect(
+      "/dashboard?message=Some error occured, please try again!",
+    );
+  }
 
-    const {data,error} = await supabase.from("checkin").insert({scheduled_check_in_date: check_in_date, scheduled_check_in_time:check_in_time,luggage_info:luggage_info});
+  return res.redirect("/dashboard?message=Check-In Scheduled Succesfully!");
+});
 
-    if(error){
-        return res.redirect("/dashboard?message=Some error occured, please try again!");
-    }
-
-    return res.redirect("/dashboard?message=Check-In Scheduled Succesfully!");
-
-})
-
-app.listen(port)
+app.listen(port);
